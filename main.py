5306cfc089dd30ab2e60760029954dc43ff1d4e6
Ammar Hussein <huswainamar@gmail.com>
2025-06-10 22:09:04 +0200

    fix cuda?

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from app.model import extract_text_from_image
 from app.utils import load_image_from_bytes
 
 for i in range(1, 9):
-    with open(f"../tests/{i}.jpg", "rb") as f:
+    with open(f"/tests/{i}.jpg", "rb") as f:
         img = load_image_from_bytes(f)
     result = extract_text_from_image(img)
     print(f"result of {i} is {result}")
