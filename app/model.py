42ad68ed7906cee262c64cf19ca0c7ade584c6f5
Ammar Hussein <huswainamar@gmail.com>
2025-06-11 10:56:22 +0200

    idk

--- app/model.py
+++ app/model.py
@@ -4,7 +4,7 @@ from deepseek_vl2.models import DeepseekVLV2ForCausalLM, DeepseekVLV2Processor
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
 
-MODEL_NAME = "deepseek-ai/deepseek-vl2-small"
+MODEL_NAME = "deepseek-ai/deepseek-vl2-tiny"
 
 processor = DeepseekVLV2Processor.from_pretrained(MODEL_NAME)
 model = DeepseekVLV2ForCausalLM.from_pretrained(MODEL_NAME, trust_remote_code=True)
@@ -12,7 +12,7 @@ model = model.to(device).eval()
 
 
 def extract_text_from_image(image):
-    prompt = "<ImageHere> Extract all the text content from this image."
+    prompt = "<ImageHere> Extract the alphanumeric characters of this image, respond with the characters only without spaces between them."
     inputs = processor(images=[image], prompt=prompt, return_tensors="pt").to(device)
 
     with torch.no_grad():
--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from app.utils import load_image_from_bytes
 
 for i in range(1, 9):
     with open(f"/tests/{i}.jpg", "rb") as f:
-        img = load_image_from_bytes(f)
+        img = load_image_from_bytes(f.read())
     result = extract_text_from_image(img)
     print(f"result of {i} is {result}")
+    with open("results.csv", "a") as f:
+        f.write(f"{i}.jpg, {result}")
