4fcbfbaa1b663d091754c9a565aa1772553cf693
Ammar Hussein <huswainamar@gmail.com>
2025-06-11 18:44:54 +0200

    clean resuly

--- bot.py
+++ bot.py
@@ -4,11 +4,14 @@ from pyrogram import Client, filters
 
 from model import extract_text
 
+print("model imported")
+
 API_ID = os.getenv("API_ID")
 API_HASH = os.getenv("API_HASH")
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 
 app = Client("myregbot", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
+print("Bot created")
 
 
 @app.on_message(filters.private & filters.user("@Sd_Ammar"))
@@ -19,9 +22,10 @@ async def ocr(client, message):
 
     # Read the image
     image_path = await client.download_media(message.photo.file_id)
-    await message.reply(image_path)
-    result = extract_text(image_path)
-    await message.reply(result.replace("<｜end▁of▁sentence｜", ""))
+    result = (
+        extract_text(image_path).replace("<｜end▁of▁sentence｜>", "").replace(" ", "")
+    )
+    await message.reply(result)
 
 
 app.run()  # Automatically start() and idle()
